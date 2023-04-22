9257f30a348e67c43278e6ffe9c57d55c388ed9d
Robert Smith <master_schmadoodle@outlook.com>
2023-04-22 18:30:11 +0100

    Minor changes to file structure

--- index.php
+++ index.php
@@ -3,37 +3,14 @@
 	define('QS_PAGE',		isset($_GET['page'])	? strtolower($_GET['page']) : 'index');
 	define('QS_SUBPAGE',	isset($_GET['subpage'])	? strtolower($_GET['subpage']) : null);
 	define('QS',			isset($_GET['q'])		? (strpos($_GET['q'], '/') ? explode('/', strtolower($_GET['q'])) : strtolower($_GET['q'])) : null);
-<<<<<<< HEAD
 	define('QS_FILE',		isset($_GET['file'])	? strtolower($_GET['file']) : "doesnt");
 	define('QS_EXT',		isset($_GET['ext'])		? strtolower($_GET['ext']) : "exist");
 	require_once('Classes.php');
 
-=======
-	define('QS_FILE',		$_GET['file']);
-	define('QS_EXT',		$_GET['ext']);
-	require_once('Classes.php');
-	print_r(QS_FILE.".".QS_EXT);
->>>>>>> 075fa4155da57fd2c4545ec592995f1d0c0f34fa
 	$cnf = new Config(__ROOT__.'/Configuration/config.ini');
 	$config = $cnf->read();
 	$db_c = new DB($config['Databases']['Central']);
 	$db_a = new DB($config['Databases']['Metrics']);
-<<<<<<< HEAD
-
-	$tools = new Tools($db_c);
-	if($db_c->num_rows(sprintf("SELECT * FROM `Domains` WHERE `Domain`='%s'", $_SERVER['SERVER_NAME'])) > 0) {
-		$website = new Website($_SERVER['SERVER_NAME'], $db_c);
-		$page = new Page(QS_PAGE, QS_SUBPAGE, QS, $db_c);
-		if($page->page_id) {
-			$theme = new Theme($website->info['Theme'], $db_c, $website->info['ID'], $page);
-			$themeinfo = $theme->info;
-			if(file_exists($file = __ROOT__."/Themes/".$themeinfo['Location']."/assets/".QS_FILE.".".QS_EXT)) {
-				header("Content-Type: " . $tools->get_mime_type(QS_FILE.".".QS_EXT) . "; charset=UTF-8;");
-				print(file_get_contents($file));
-			} elseif(file_exists($file = __ROOT__."/Updater/".QS_FILE.".".QS_EXT)) {
-				include_once($file);
-			} else {
-=======
 	$tools = new Tools($db_c);
 	if($db_c->num_rows(sprintf("SELECT * FROM `Domains` WHERE `Domain`='%s'", $_SERVER['SERVER_NAME'])) > 0) {
 		print("Here: 1");
@@ -52,17 +29,12 @@
 				require_once($file);
 			} else {
 				print("Here: 5");
->>>>>>> 075fa4155da57fd2c4545ec592995f1d0c0f34fa
 				print($theme->generate());
 			}
 		} else {
 			header('HTTP/1.0 404 Not Found');
 		}
 	} else {
-<<<<<<< HEAD
-		header('HTTP/1.0 500 Internal Server Error');
-=======
 		header('HTTP/1.0 404 Not Found');
->>>>>>> 075fa4155da57fd2c4545ec592995f1d0c0f34fa
 	}
 ?>
