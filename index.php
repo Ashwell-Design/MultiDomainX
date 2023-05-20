f222b973cc4215b1ac7e95f103a4138f59e839cb
Robert Smith <master_schmadoodle@outlook.com>
2023-05-20 11:16:51 +0100

    Minor changes to fix the git_webhook

--- index.php
+++ index.php
@@ -24,7 +24,8 @@ error_reporting(E_ALL);
 		if($page->page_id) {
 			$theme = new Theme($website->info['Theme'], $db_c, $website->info['ID'], $page, $config['Theme']['Default']);
 			$themeinfo = $theme->info;
-			if(file_exists($file = __ROOT__."/".QS_FILE.".".QS_EXT)) {
+			print($file = __ROOT__."/".QS_FILE.".".QS_EXT);
+			if(file_exists($file)) {
 				header("Content-Type: " . $tools->get_mime_type(QS_FILE.".".QS_EXT) . "; charset=UTF-8;");
 				require_once($file);
 			} else {
