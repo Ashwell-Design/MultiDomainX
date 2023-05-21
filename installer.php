c8e70c3466df589f016453a8738a66cb537cfc76
Robert Smith <master_schmadoodle@outlook.com>
2023-05-21 15:50:46 +0100

    Added functionality

--- installer.php
+++ installer.php
@@ -218,6 +218,47 @@
 		/* INTRODUCTION */
 		.cont .int { }
 	</style>
+</head>
+<body>
+	<div class="cont">
+		<div class="elem">
+			<div class="c one"></div>
+			<div class="c two"></div>
+			<div class="c three"></div>
+		</div>
+		<div class="frm">
+			<section class="card card-popup open" id="welcome">
+				<h1>Welcome</h1>
+				<div class="btn-deck">
+					<button type="button" class="btn" data-method="open" data-target="#license">NEXT</button>
+				</div>
+			</section>
+			<section class="card card-slideover" id="license">
+				<h1>License agreeement</h1>
+				<div class="btn-deck">
+					<button type="button" class="btn btn-close" data-method="close" data-target="#welcome"></button>
+					<button type="button" class="btn" data-method="open" data-target="#card-3">NEXT</button>
+				</div>
+			</section>
+			<section class="card card-slideover" id="activation">
+				<h1>Activation</h1>
+				<div class="btn-deck">
+					<button type="button" class="btn btn-close" data-method="close" data-target="#license"></button>
+					<button type="button" class="btn" data-method="open" data-target="#card-3">NEXT</button>
+				</div>
+			</section>
+			<section class="card card-slideover" id="card-3">
+				<h1>Installing</h1>
+				<div class="btn-deck">
+					
+				</div>
+			</section>
+		</div>
+		<div class="pb" style="--progress=0%; --progress-text='0%';"></div>
+		<div class="int">
+
+		</div>
+	</div>
 	<script>
 		updateProgress = (mode, value) => {
 			if(value < 101) {
@@ -267,45 +308,4 @@
 			})
 		);
 	</script>
-</head>
-<body>
-	<div class="cont">
-		<div class="elem">
-			<div class="c one"></div>
-			<div class="c two"></div>
-			<div class="c three"></div>
-		</div>
-		<div class="frm">
-			<section class="card card-popup open" id="welcome">
-				<h1>Welcome</h1>
-				<div class="btn-deck">
-					<button type="button" class="btn" data-method="open" data-target="#license">NEXT</button>
-				</div>
-			</section>
-			<section class="card card-slideover" id="license">
-				<h1>License agreeement</h1>
-				<div class="btn-deck">
-					<button type="button" class="btn btn-close" data-method="close" data-target="#welcome"></button>
-					<button type="button" class="btn" data-method="open" data-target="#card-3">NEXT</button>
-				</div>
-			</section>
-			<section class="card card-slideover" id="activation">
-				<h1>Activation</h1>
-				<div class="btn-deck">
-					<button type="button" class="btn btn-close" data-method="close" data-target="#license"></button>
-					<button type="button" class="btn" data-method="open" data-target="#card-3">NEXT</button>
-				</div>
-			</section>
-			<section class="card card-slideover" id="card-3">
-				<h1>Installing</h1>
-				<div class="btn-deck">
-					
-				</div>
-			</section>
-		</div>
-		<div class="pb" style="--progress=0%; --progress-text='0%';"></div>
-		<div class="int">
-
-		</div>
-	</div>
 </body>
