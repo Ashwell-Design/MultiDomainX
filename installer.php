d12ed95d76f1361c8275038fa82ae0ddd54d2522
Robert Smith <master_schmadoodle@outlook.com>
2023-05-21 15:48:52 +0100

    Added functionality and styling

--- installer.php
+++ installer.php
@@ -1,6 +1,272 @@
 <!DOCTYPE html>
 <head>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap');
+		body{
+			font-family: 'Comfortaa';
+			padding: unset;
+			margin: unset;
+			background-image: url('https://i.pinimg.com/originals/f9/d1/df/f9d1df4811f6fb6703941b834d123e70.jpg');
+			background-position: center center;
+			background-repeat: no-repeat;
+			background-size: cover;
+		}
+		/* CONTAINER */
+		.cont {
+			display: block;
+			width: 100vw;
+			height: 100vh;
+			overflow: hidden;
+		}
+		.cont::before {
+			content: "";
+			background: #16ccfe;
+			height: 100vh;
+			width: 100vw;
+			opacity: 0.3;
+			position: fixed;
+			z-index: -5;
+		}
+		/* PROGRESS BAR */
+		.cont .pb::before,
+		.cont .pb::after {
+			position: fixed;
+			display: inherit;
+			bottom: inherit;
+			height: inherit;
+		}
+		.cont .pb::before {
+			content: "";
+			background-color: silver;
+			opacity: 0.5;
+			width: 100vw;
+			z-index: -1;
+		}
+		.cont .pb {
+			--progress: ;
+			--progress-text: '';
+			position: absolute;
+			display: block;
+			height: 5px;
+			bottom: 0;
+			opacity: 1;
+			background-color: #00f1cf;
+			width: var(--progress);
+		}
+		.cont .pb::after {
+			left: 0;
+			bottom: 2rem;
+			height: auto;
+			left: 2rem;
+			content: var(--progress-text);
+			color: #00f1cf;
+			z-index: -1;
+			font-size: 2rem;
+		}
+		/* ELEMENTS */
+		.elem .c { }
+		.elem .c .1 { }
+		.elem .c .2 { }
+		.elem .c .3 { }
+		/* FORM */
+		.cont .frm {
+			width: 50%;
+			height: auto;
+			position: fixed;
+			top: 50%;
+			left: 50%;
+			min-height: 50%;
+			transform: translate(-50%, -50%);
+		}
+		.card {
+		--anim-dura: .6s;
+		--anim-dura-alt: .55s;
+		
+		display: flex;
+		flex-flow: column wrap;
+		justify-content: space-between;
+		
+		opacity: 0;
+		
+		width: calc(100% - 2rem);
+		max-width: 400px;
+		height: 600px;
+		max-height: calc(100% - 8rem);
+		
+		padding: 1rem;
+		
+		position: fixed!important;
+		top: 50%;
+		left: 50%;
+		
+		background: white;
+		box-shadow: 0 0 6px #0001, 0 6px 6px #0003;
+		
+		border-radius: .5rem;
+		
+		transform: translate(-50%, -50%);
+		
+		transition: all var(--anim-dura) cubic-bezier(0.68, -0.55, 0.265, 1.55);
+		transition-delay: 0s;
+		}
+
+		.card.open {
+		opacity: 1;
+		
+		transform: translate(-50%, -50%);
+		
+		transition-delay: 0s;
+		}
+
+		.card:not(.open.idle) {
+		transition-delay: var(--anim-dura);
+		}
+
+		.card.open.idle {
+		box-shadow: 0 0 2px #0001, 0 2px 2px #0002;
+		
+		transform: translate(-50%, -38%) scale(.8);
+		
+		transition-delay: 0s;
+		}
+
+		.card.open:after {
+		content: '';
+		
+		opacity: 0;
+		
+		width: 100%;
+		height: 100%;
+		
+		position: absolute;
+		top: 100vh;
+		left: 100vw;
+		
+		background: #0001;
+		
+		border-radius: inherit;
+		
+		transition: opacity var(--anim-dura) cubic-bezier(0.68, -0.55, 0.265, 1.55);
+		transition-delay: var(--anim-dura-alt);
+		}
+
+		.card.open.idle:after {
+		top: 0;
+		left: 0;
+		
+		opacity: 1;
+		
+		transition-delay: var(--anim-dura-alt);
+		}
+
+		.card.card-popup:not(.open) {
+		transform: translate(-50%, -50%) scale(.8);
+		}
+
+		.card.card-slideover {
+		transition-delay: var(--anim-dura);
+		}
+
+		.card.card-slideover:not(.open) {
+		transform: translate(-50%, 100%);
+		
+		transition-delay: 0s;
+		}
+
+		.card h1 {
+		font-size: 2rem;
+		}
+
+		.card .btn-deck {
+		display: flex;
+		}
+
+		.card .btn-deck button:not(:last-child) {
+		margin-right: 1rem;
+		}
+
+		.card .btn-deck button ~ button {
+		padding: 0 1rem;
+		}
+
+		@keyframes mymove {
+			000% {
+				rotate: 0deg;
+				top: 0px;
+				left: 0px;
+			}
+			049% {
+				rotate: -16deg;
+				top: -100%;
+				left: -50%;
+			}
+			050% {
+				z-index: -50;
+			}
+			051% {
+				rotate: -16deg;
+				top: -100%;
+				left: -50%;
+			}
+			100% {
+				rotate: 0deg;
+				top: 0px;
+				left: 0px;
+			}
+		}
+		/* INTRODUCTION */
+		.cont .int { }
+	</style>
+	<script>
+		updateProgress = (mode, value) => {
+			if(value < 101) {
+				var newProgress;
+				const element = document.querySelector('.pb');
+				const computedStyle = getComputedStyle(element);
+				var progress = parseInt(computedStyle.getPropertyValue('--progress'));
+				switch(mode) {
+					case 'add':
+						newProgress = progress+value;
+						if(newProgress>100) newProgress=100;
+						break;
+					case 'set':
+						newProgress = value;
+						break;
+					case 'sub':
+						newProgress = progress-value;
+						if(newProgress<0) newProgress=0;
+						break;
+				}
+				element.style.setProperty('--progress', `${newProgress}%`);
+				element.style.setProperty('--progress-text', `'${newProgress}%'`);
+			}
+		}
+		updateProgress('set', 37)
+
+
+
+		let wrapper = document.querySelector(".frm");
+		let cards = wrapper.querySelectorAll(".card");
+		const toggleCard = (card, prevCard) => (card.classList.toggle("open"), prevCard && prevCard.classList.toggle("idle"));
+		cards.forEach(card =>
+			card.addEventListener("click", e => {
+				const el = e.target;
+
+				if (el.tagName === "BUTTON") {
+					const method = el.dataset.method;
+
+					const target = wrapper.querySelector(el.dataset.target);
+					const parent = el.closest(".card");
+
+					if (method === "open")
+						toggleCard(target, parent);
+					else if (method === "close")
+						toggleCard(parent, target);
+				}
+			})
+		);
+	<script>
 </head>
 <body>
 	<div class="cont">
