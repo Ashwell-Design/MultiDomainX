c401df93fe2b32e767fb060b2e21af7f623e06da
Robert Smith <master_schmadoodle@outlook.com>
2023-05-22 21:47:29 +0100

    Minor changes to installer UI

--- installer.php
+++ installer.php
@@ -178,7 +178,10 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.min.js" integrity="sha512-1/RvZTcCDEUjY/CypiMz+iqqtaoQfAITmNSJY17Myp4Ms5mdxPS5UV7iOfdZoxcGhzFbOm6sntTKJppjvuhg4g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	<script>
 		//
-			const welcomeModal = new bootstrap.Modal('#welcome');
+			const welcomeModal = new bootstrap.Modal('#welcome', {
+				show: true,
+				backdrop: static,
+			});
 		// PROGRESS BAR
 		updateProgress = (mode, value) => {
 			if(value < 101) {
