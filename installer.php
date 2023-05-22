5d8e14c76256d86a1a55d6664a823c4e3d489f23
Robert Smith <master_schmadoodle@outlook.com>
2023-05-22 22:42:36 +0100

    Minor changes to installer UI

--- installer.php
+++ installer.php
@@ -112,7 +112,7 @@
 		</div>
 	</div>
 	
-	<div class="modal modal-xl fade show" tabindex="-1" role="dialog" aria-hidden="true" aria-labelledby="welcomeModalLabel" id="welcome">
+	<div class="modal modal-xl fade" tabindex="-1" role="dialog" aria-hidden="true" aria-labelledby="welcomeModalLabel" id="welcome">
 		<div class="modal-dialog modal-fullscreen-xl-down" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
