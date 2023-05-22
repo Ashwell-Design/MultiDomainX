164df2c9d6ebaa9906c41063af2ad91fac61e070
Robert Smith <master_schmadoodle@outlook.com>
2023-05-22 23:07:03 +0100

    Minor changes to installer UI

--- installer.php
+++ installer.php
@@ -90,8 +90,8 @@
 				-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
 				background-color: #555;
 			}
-			.show {
-				display: block;
+			.license pre {
+				width: fit-content;
 			}
 		/* INTRODUCTION */
 			/*
