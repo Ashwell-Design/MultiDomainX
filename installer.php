6d25fd7c332b842f9eec1a4b76765e01a186a291
Robert Smith <master_schmadoodle@outlook.com>
2023-05-25 11:51:41 +0100

    Minor changes

--- installer.php
+++ installer.php
@@ -9,11 +9,10 @@
 			width: 5px;
 			background-color: transparent;
 		}
-		*::-webkit-scrollbar-track {
-			-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+		*::-webkit-scrollbar-thumb {
 			border-radius: 10px;
+			-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
 			background-color: #F5F5F5;
-
 		}
 		body{
 			font-family: 'Comfortaa';
