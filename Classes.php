ac8fded64a695029eaa24b3aa5a4fa1114fee15b
Robert Smith <master_schmadoodle@outlook.com>
2023-05-28 13:25:05 +0100

    Fixed theme info.json

--- Classes.php
+++ Classes.php
@@ -208,7 +208,6 @@
 		public function __construct($theme_id, $db, $dom_id, $page, $d_theme) {
 			$this->db = $db;
 			$this->info = $db->assoc(sprintf("SELECT * FROM `Themes` WHERE `id`='%s'", $theme_id));
-			print_r($this->info);
 			$this->theme_id = $theme_id;
 			$this->domain_id = $dom_id;
 			$this->page = $page;
