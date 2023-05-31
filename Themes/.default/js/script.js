7d2558c057686c25de3672fb2be9ff04153684b1
Robert Smith <master_schmadoodle@outlook.com>
2023-05-31 11:40:39 +0100

    Minor changes to the table preloading

--- Themes/.default/js/script.js
+++ Themes/.default/js/script.js
@@ -27,7 +27,6 @@ function loadTable(elem) {
 		extension = $(elem).attr('preload-attributes');
 		const [table, col_ids, buttonString] = extension.split('-', 3);
 		const cols = col_ids.split("");
-		console.log(cols);
 		const thead = $(elem).children('thead')[0];
 		const tbody = $(elem).children('tbody')[0];
 
@@ -51,7 +50,7 @@ function loadTable(elem) {
 				var stmt = db.prepare("PRAGMA table_info("+table+")");
 				stmt.run()
 				while (stmt.step()) {
-					if(cols.includes(i)) {
+					if(cols.includes(i.toString())) {
 						var row = stmt.getAsObject();
 						var columnName = row.name;
 						var table_row = $('<th></th>').html(columnName);;
