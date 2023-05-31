eebf9bec21db8c4757a2168268b41769b8959f01
Robert Smith <master_schmadoodle@outlook.com>
2023-05-31 13:48:04 +0100

    Minor changes to the table preloading

--- Themes/.default/js/script.js
+++ Themes/.default/js/script.js
@@ -86,7 +86,7 @@ $(document).ready(async function() {
 				const elem = $(this);
 
 				window[command](elem, function(elem) {
-					$(elem)[0].attr('preload-status', 'Loaded');
+					($(elem)[0]).attr('preload-status', 'Loaded');
 					resolve();
 				});
 			});
