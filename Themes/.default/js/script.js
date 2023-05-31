abfcf528714203f994845a7758522d4be8ec5464
Robert Smith <master_schmadoodle@outlook.com>
2023-05-31 09:36:49 +0100

    Added table preload

--- Themes/.default/js/script.js
+++ Themes/.default/js/script.js
@@ -23,6 +23,9 @@ function changeLanguage(lang) {
 // Loads a table
 function loadTable(elem) {
 	elem = elem[0];
+	console.log(elem);
+	console.log(elem[0]);
+	console.log($(elem));
 	extension = $(elem).attr('preload-attributes')
 	var [table, cols, buttonString] = extension.split('-', 3);
 	var tbody = $(elem).children('>tbody');
