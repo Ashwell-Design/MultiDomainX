92b4ef8e91a2476407e0db4b8ccc0202db32979b
Robert Smith <master_schmadoodle@outlook.com>
2023-06-02 16:29:19 +0100

    Attempting to change the url structure from primary / secondary to permalink

--- Classes.php
+++ Classes.php
@@ -290,8 +290,8 @@
 								[$seccode, $string] = explode(':', $string);
 								$tools = new Tools($this->db);
 								if ($this->db->num_rows($sql = sprintf("SELECT `Type`, `File` FROM `Sections` WHERE `Code`='%s'", $seccode)) == 1) {
-									print_r(file_exists("{$this->theme_path}/sections/$type/$file.html"));
 									[$type, $file] = $this->db->array($sql);
+									print_r(file_exists("{$this->theme_path}/sections/$type/$file.html"));
 									if(file_exists("{$this->theme_path}/sections/$type/$file.html")) {
 										$out .= $tools->ParseShortcodes(
 											file_get_contents("{$this->theme_path}/sections/$type/$file.html"),
